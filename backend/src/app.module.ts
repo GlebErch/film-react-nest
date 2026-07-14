5cb0edce7e85e59b703b0f866d51b42f324892c1
Gleb E <gleb.erchenko.81@yandex.ru>
2026-07-14 16:28:52 +0500

    фикс версии typeorm для автотестов

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,4 @@
+import './crypto-polyfill';
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
